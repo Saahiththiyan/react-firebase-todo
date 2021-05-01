99a7c911bd8bd3f9a2d6a51d00dcbef24af01459
saahi <saahimathi@gmail.com>
2021-05-01 20:24:16 +0530

    feat: add loading spinner

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion";
 import "./App.css";
 import Form from "./components/Form";
 import TodoItem from "./components/TodoItem";
+import Spinner from "./components/Spinner";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     const todoRef = firebase.database().ref("Todo");
     todoRef.on("value", (snapshot) => {
       const todos = snapshot.val();
@@ -17,6 +20,7 @@ function App() {
         todoList.push({ id, ...todos[id] });
       }
       setTodoList(todoList);
+      setIsLoading(false);
     });
   }, []);
   return (
@@ -34,8 +38,9 @@ function App() {
               Add Todo
             </motion.p>
             <Form />
+            {isLoading && <Spinner />}
             <AnimatePresence>
-              {todoList
+              {todoList.length > 0
                 ? todoList.map((todo) => {
                     return <TodoItem key={todo.id} todo={todo} />;
                   })
